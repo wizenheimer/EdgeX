11d64c6648c975004dc4249fa08b3e02381e401a
kn4y4n <kn4y4nnn@gmail.com>
2023-03-03 11:34:27 +0530

    feat: add auth

--- courses/models.py
+++ courses/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
 
 class Subject(models.Model):
     title = models.CharField(max_length=250)
@@ -41,3 +44,46 @@ class Module(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Content(models.Model):
+    module = models.ForeignKey(
+        Module,
+        related_name="contents",
+        on_delete=models.CASCADE,
+        limit_choices_to={"model__in": ("text", "video", "image", "file")},
+    )
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey("content_type", "object_id")
+
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(
+        get_user_model(), related_name="%(class)s_related", on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+class Text(ItemBase):
+    content = models.TextField()
+
+
+class File(ItemBase):
+    content = models.FileField(upload_to="files")
+
+
+class Image(ItemBase):
+    content = models.ImageField(upload_to="images")
+
+
+class Video(ItemBase):
+    content = models.URLField()
